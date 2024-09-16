1d4c245bf10369e2822ab0fbf2e082416585a862
Joaquin <clcflecha@gmail.com>
2024-09-15 20:19:06 -0400

    useLayoutEffect

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,14 @@ import { createRoot } from 'react-dom/client'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
-import { FocusScreen } from './04-useRef/FocusScreen'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
 
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    // <FormWithCustomHook />
-    <FocusScreen />
+    <Layout />
   // </StrictMode>,
 )
